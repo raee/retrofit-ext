694a56c238a867adf29a1d9dd14d677da3713fe0
rae <798404374@qq.com>
2017-04-25 14:45:31 +0800

    fork from https://github.com/raee/retrofit/tree/master/retrofit-converters/gson commit 1cc726f6a5efcc9e0d5e4b740acee62985d9d5c5

---
+++ gson/src/androidTest/java/retrofit2/converter/gson/ExampleInstrumentedTest.java
@@ -0,0 +1,49 @@
+package retrofit2.converter.gson;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+
+        GsonConverterFactory.create(new Gson(), new IGsonResponseBodyConverter() {
+            @Override
+            public Converter<ResponseBody, ?> responseBodyConverter(Gson gson, TypeAdapter<?> adapter, Type type, Annotation[] annotations) {
+                return new MyGsonConverter<>(gson, adapter, type, annotations);
+            }
+        });
+
+    }
+
+
+    private class MyGsonConverter<T> extends GsonResponseBodyConverter<T> {
+
+        MyGsonConverter(Gson gson, TypeAdapter<T> adapter, Type type, Annotation[] annotations) {
+            super(gson, adapter, type, annotations);
+        }
+
+        @Override
+        public T onResponseInterceptor(ResponseBody body, TypeAdapter<T> adapter, Type type, Annotation[] annotations) throws IOException {
+            return super.onResponseInterceptor(body, adapter, type, annotations);
+        }
+    }
+}

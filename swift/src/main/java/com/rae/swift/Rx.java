050e7c495bea75697abe7c5d6dba964a668df8b4
rae <798404374@qq.com>
2017-05-05 19:16:34 +0800

    添加工具类

---
+++ swift/src/main/java/com/rae/swift/Rx.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2017.
+ */
+
+package com.rae.swift;
+
+import android.text.TextUtils;
+
+import java.util.Collection;
+
+/**
+ * 工具类
+ * Created by ChenRui on 2017/5/5 0005 12:02.
+ */
+public final class Rx {
+
+    public static <E> boolean isEmpty(Collection<E> list) {
+        return list == null || list.isEmpty();
+    }
+
+    public static boolean isEmpty(String text) {
+        return TextUtils.isEmpty(text);
+    }
+}
